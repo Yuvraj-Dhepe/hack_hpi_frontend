5ad1854c78ac245abb6e5a6236b2ba2216a53bbc
Fynn <fynn_kroeger@web.de>
2025-03-21 21:24:37 +0100

    dont show header

--- TinnyHPI/app/_layout.tsx
+++ TinnyHPI/app/_layout.tsx
@@ -29,9 +29,8 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ title: "Home" }} />
-        <Stack.Screen name="tips" options={{ title: "Tips" }} />        
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
